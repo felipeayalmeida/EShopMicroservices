93ef2c21e942d6cd6a3d5833816426f47aaa221b
feaya <feaya@DESKTOP-GAU99ON>
2025-03-15 18:51:58 -0300

    commit for wsl migration

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+// Generic for Commands without Response
+public interface ICommand : IRequest<Unit>
+{
+}
+
+// Generic for Commands with Response
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{
+}
+
