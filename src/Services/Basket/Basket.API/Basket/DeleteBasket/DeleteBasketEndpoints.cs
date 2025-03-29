7ffba1cf1e117223aa52d7faaf23218a34fd3099
feaya <feaya@DESKTOP-GAU99ON>
2025-03-29 12:43:11 -0300

    ending basketapi core with postgres infra and exception handler

---
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoints.cs
@@ -0,0 +1,24 @@
+﻿namespace Basket.API.Basket.DeleteBasket;
+
+public record DeleteBasketRequest(string UserName);
+public record DeleteBasketResponse(bool IsSuccess);
+public class DeleteBasketEndpoints : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/basket/{username}", async (string username ,ISender sender) =>
+        {
+            var command = new DeleteBasketCommand(username);
+            var result = await sender.Send(command);
+            var response = result.Adapt<DeleteBasketResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("DeleteProduct")
+        .Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Delete Product")
+        .WithDescription("Delete Product");
+    }
+}
