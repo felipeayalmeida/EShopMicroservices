f7173ecf94849e26b6ce06d4a54ee657f25acb5b
feaya <feaya@DESKTOP-GAU99ON>
2025-05-11 18:24:27 -0300

    finishing infra layer

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
 
 namespace Ordering.Infrastructure;
 
@@ -8,11 +8,16 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructureServices (this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("Database");
+        // Add services to the container.
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
 
-        services.AddDbContext<ApplicationDbContext>(options =>
+        services.AddDbContext<ApplicationDbContext>((sp, options) =>
         {
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
             options.UseSqlServer(connectionString);
         });
+
 
         //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
--- src/Services/Ordering/Ordering.Infrastructure/GlobalUsings.cs
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsings.cs
@@ -1,3 +1,8 @@
 ﻿global using Ordering.Infrastructure.Data;
 global using Microsoft.EntityFrameworkCore;
-
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.Extensions.DependencyInjection;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
+global using Ordering.Domain.Abstractions;
+global using Ordering.Infrastructure.Data.Interceptors;
